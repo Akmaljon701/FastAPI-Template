9f66c3b50e9d42142c872fa42c76a18c333c2e34
Akmaljon <akmalkhanever@gmail.com>
2024-01-15 22:05:38 +0500

    Update .gitignore and remove .idea/ from git

--- db.py
+++ db.py
@@ -2,11 +2,21 @@ import uvicorn
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # database url
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root@localhost:3306/template'
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-SECRET_KEY = 'SOME-SECRET-KEY'
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
